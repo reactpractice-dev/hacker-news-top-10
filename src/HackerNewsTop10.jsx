03fe5f64e25a33c6bbcc3541d65998a0ddb7684b
Corina Udrescu <corinutz@gmail.com>
2024-06-12 07:40:43 +0200

    fetch article ids

--- src/HackerNewsTop10.jsx
+++ src/HackerNewsTop10.jsx
@@ -1,4 +1,17 @@
+import { useEffect } from "react";
+
+const fetchTop10Articles = async () => {
+  const topArticlesResponse = await fetch(
+    "https://hacker-news.firebaseio.com/v0/topstories.json"
+  );
+  const topArticleIds = await topArticlesResponse.json();
+  console.log(topArticleIds);
+};
+
 const HackerNewsTop10 = () => {
+  useEffect(() => {
+    fetchTop10Articles();
+  }, []);
   return <div>Build your component here!</div>;
 };
 
