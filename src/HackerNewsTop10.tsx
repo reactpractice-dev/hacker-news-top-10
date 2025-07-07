bf76507d493d0027f75cfd7f87edd817961ede23
Corina Udrescu <corinutz@gmail.com>
2025-07-07 16:15:24 +0200

    show top 10

--- src/HackerNewsTop10.tsx
+++ src/HackerNewsTop10.tsx
@@ -1,35 +1,71 @@
 import { useQuery } from "@tanstack/react-query";
 
+type HackerNewsArticle = {
+  by: string;
+  descendants: number;
+  id: number;
+  kids: number[];
+  score: number;
+  time: number;
+  title: string;
+  type: "story";
+  url: string;
+};
+
+const fetchTop10ArticlesWithDetails = async () => {
+  const response = await fetch(
+    "https://hacker-news.firebaseio.com/v0/topstories.json"
+  );
+  if (!response.ok) {
+    throw new Error("Network response was not ok");
+  }
+
+  const data = await response.json();
+  const top10Ids = data.slice(0, 10);
+  const articlesWithDetails = await Promise.all(
+    top10Ids.map(async (id: number) => {
+      const articleResponse = await fetch(
+        `https://hacker-news.firebaseio.com/v0/item/${id}.json`
+      );
+      if (!articleResponse.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return articleResponse.json();
+    })
+  );
+
+  return articlesWithDetails;
+};
+
 const HackerNewsTop10 = () => {
   const { data, isLoading, error } = useQuery({
     queryKey: ["topStories"],
-    queryFn: async () => {
-      const response = await fetch(
-        "https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty"
-      );
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-      return response.json();
-    },
+    queryFn: fetchTop10ArticlesWithDetails,
   });
   return (
-    <ul>
-      {data?.slice(0, 10).map((id: number) => (
-        <li key={id}>
-          <a
-            href={`https://news.ycombinator.com/item?id=${id}`}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {id}
-          </a>
-        </li>
-      ))}
-      {isLoading && <li>Loading...</li>}
-      {error && <li>Error loading data: {error.message}</li>}
-      {data && data.length === 0 && <li>No stories found.</li>}
-    </ul>
+    <>
+      <h1>Hacker News Top 10 Articles</h1>
+      <ul>
+        {data?.map((article: HackerNewsArticle) => (
+          <li key={article.id}>
+            <a
+              href={`https://news.ycombinator.com/item?id=${article.id}`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {article.title}
+            </a>
+            <br />
+            <span>
+              by {article.by} - {article.score} points
+            </span>
+          </li>
+        ))}
+        {isLoading && <li>Loading...</li>}
+        {error && <li>Error loading data: {error.message}</li>}
+        {data && data.length === 0 && <li>No stories found.</li>}
+      </ul>
+    </>
   );
 };
 
